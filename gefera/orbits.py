b937d4f436aa56e424570b670099cd56ba87da67
pcosta19 <pcosta19@uw.edu>
2023-05-10 14:28:31 -0700

    Changed which parameters are taken by PrimaryOrbit and SatelliteOrbit and then calculates the orginal params from the new ones

--- gefera/orbits.py
+++ gefera/orbits.py
@@ -46,8 +46,22 @@ class PrimaryOrbit(Orbit):
         w: Argument of periastron (in radians)
         i: Inclination (in radians)
     """
+    def def_e(ecosw,esinw):
+        return np.sqrt(ecosw**2+esinw**2)
+    def def_a(p1,b1,T,ecosw,esinw):
+        return np.sqrt((1-b1**2)/(np.sin(Pi*T/p1*(1+esinw)**2/(1-def_e(ecosw,esinw)**2)**(3/2))**2)+b1**2)*(1+esinw)/(1-def_e(ecosw,esinw)**2)
+    def def_i1(b1,p1,T,ecosw,esinw):
+        return np.arccos(b1/def_a(p1,b1,T,ecosw,esinw)*(1+esinw)/(1-def_e(ecosw,esinw)**2))
+    def def_w(ecosw,esinw):
+        return np.arctan(esinw/ecosw)
+    def def_t1(t0,p,ecosw,esinw):
+        return t0+p*np.sqrt(1-def_e(ecosw,esinw)**2)/(2*Pi)*(def_e(ecosw,esinw)*np.sin(1.5*Pi-def_w(ecosw,esinw))/(1+def_e(ecosw,esinw)*np.cos(1.5*Pi-def_w(ecosw,esinw))) - 2/np.sqrt(1-def_e(ecosw,esinw)**2)*math.atan2(np.sqrt(1-def_e(ecosw,esinw)**2)*np.tan(0.75*Pi-0.5*def_w(ecosw,esinw)),(1+def_e(ecosw,esinw))))
     
-    def __init__(self, a, t, e, p, w, i):
+    def __init__(self, T, t0, esinw, ecosw, p, b):
+        a = def_a(p,b,T,ecosw,esinw)
+        t = def_t1(t0,p,ecosw,esinw)
+        e = def_e(ecosw,esinw)
+        w = def_w(ecosw,esinw)
         super().__init__(a, t, e, p, w, i)
         
     def pdict(self):
@@ -69,8 +83,21 @@ class SatelliteOrbit(Orbit):
         i: Inclination (in radians)
         m: Moon/planet mass ratio
     """
+    #definitions of old parameter set in terms of new parameters
+    def def_e(ecosw,esinw):
+        return np.sqrt(ecosw**2+esinw**2)
+    def def_t2(phi,p2):
+        return phi*p2/(2*Pi)
+    def def_w(ecosw,esinw):
+        return np.arctan(esinw/ecosw)
+    def def_i2(b2,a2,ecosw,esinw):
+        return np.arccos(b2/a2*(1+esinw)/(1-def_e(ecosw,esinw)**2))
     
-    def __init__(self, a, t, e, p, o, w, i, m):
+    def __init__(self, a, phi, esinw, ecosw, o, p, b, m):
+        e=def_e(ecosw,esinw)
+        t=def_t2(phi,p)
+        w=def_w(ecosw,esinw)
+        i=def_i2(b,a,ecosw,esinw)
         super().__init__(a, t, e, p, w, i)
         self.o = o
         self.m = m
