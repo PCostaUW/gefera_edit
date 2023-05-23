ad673bb0a39c3906c89f886d1b41bd991e0799a5
pcosta19 <pcosta19@uw.edu>
2023-05-23 12:40:52 -0700

    changed the expression for grad['P1']

--- gefera/systems.py
+++ gefera/systems.py
@@ -247,7 +247,7 @@ class System:
             dfdm2 = grad['m2']
             dfdr2 = grad['r2']
             dfda2 = grad['a2']
-            dfdP1 = grad['p1']
+            
             dfdP2 = grad['p2']
             dfdr1 = grad['r1']
             
@@ -279,6 +279,7 @@ class System:
             b2 = new_params['b2']
             
             dfdT = (grad['i1']-grad['a1']*dbdi(ap,ip,ep,wp)/dbda(ap,ip,ep,wp))/(dTdi(pp,ap,wp,ep,ip)-dTda(pp,ap,wp,ep,ip)*dbdi(ap,ip,ep,wp)/dbda(ap,ip,ep,wp))
+            dfdP1 = dfdT*dTdP(pp,ap,wp,ep,ip) + dfdt0*dt0dP(tp,ep,pp,wp)
             dfdb1 = (grad['i1']-dfdT*dTdi(pp,ap,wp,ep,ip))/dbdi(ap,ip,ep,wp)
             dfdphi = grad['t2']/dphidt(tm,em,pm,wm)
             dfdb2 = grad['i2']*di2db(am,new_params['e2sinw2'],new_params['e2cosw2'],new_params['b2'])
