b0f943bf816b60bb9098e933d98a5625c4049d8a
pcosta19 <pcosta19@uw.edu>
2023-05-23 12:42:34 -0700

    undercase p in dTdp

--- gefera/systems.py
+++ gefera/systems.py
@@ -279,7 +279,7 @@ class System:
             b2 = new_params['b2']
             
             dfdT = (grad['i1']-grad['a1']*dbdi(ap,ip,ep,wp)/dbda(ap,ip,ep,wp))/(dTdi(pp,ap,wp,ep,ip)-dTda(pp,ap,wp,ep,ip)*dbdi(ap,ip,ep,wp)/dbda(ap,ip,ep,wp))
-            dfdP1 = dfdT*dTdP(pp,ap,wp,ep,ip) + dfdt0*dt0dP(tp,ep,pp,wp)
+            dfdP1 = dfdT*dTdp(pp,ap,wp,ep,ip) + dfdt0*dt0dp(tp,ep,pp,wp)
             dfdb1 = (grad['i1']-dfdT*dTdi(pp,ap,wp,ep,ip))/dbdi(ap,ip,ep,wp)
             dfdphi = grad['t2']/dphidt(tm,em,pm,wm)
             dfdb2 = grad['i2']*di2db(am,new_params['e2sinw2'],new_params['e2cosw2'],new_params['b2'])
