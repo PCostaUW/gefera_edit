8656a47643c20fa0da7ca2c6df67bcbe589a122d
pcosta19 <pcosta19@uw.edu>
2023-05-28 13:09:09 -0700

    changed expressions for t0 and tp

--- gefera/reparam.py
+++ gefera/reparam.py
@@ -42,7 +42,7 @@ def i1(b1,p1,T,ecosw,esinw):
         esinw: component of eccentricity
     '''
     
-    return np.arccos(b1/a1(p1,b1,T,ecosw,esinw)*(1+esinw)/(1-ecc(ecosw,esinw)**2))
+    return np.arccos(b1/np.sqrt((1-b1**2)/(np.sin(Pi*T/p1*(1+esinw)**2/(1-ecc(ecosw,esinw)**2)**(3/2))**2)+b1**2))
 
 def i2(b2,a2,ecosw,esinw):
     '''Calculates the inclination of the moon's orbit.
@@ -66,7 +66,7 @@ def t1(t0,p,ecosw,esinw):
         ecosw: component of eccentricity
         esinw: component of eccentricity
     '''
-    return t0+p*np.sqrt(1-ecc(ecosw,esinw)**2)/(2*Pi)*(ecc(ecosw,esinw)*np.sin(1.5*Pi-omega(ecosw,esinw))/(1+ecc(ecosw,esinw)*np.cos(1.5*Pi-omega(ecosw,esinw))) - 2/np.sqrt(1-ecc(ecosw,esinw)**2)*atan2(np.sqrt(1-ecc(ecosw,esinw)**2)*np.tan(0.75*Pi-0.5*omega(ecosw,esinw)),(1+ecc(ecosw,esinw))))
+    return t0+p*np.sqrt(1-ecc(ecosw,esinw)**2)/(2*Pi)*(-ecosw/(1-esinw) - 2/np.sqrt(1-ecc(ecosw,esinw)**2)*atan2(np.sqrt(1-ecc(ecosw,esinw)**2)*np.tan(0.75*Pi-0.5*omega(ecosw,esinw)),(1+ecc(ecosw,esinw))))
 
 #reparameterizing omega,t_p to t0
 #this is used to calculate the mid-transit times of the barycenter about the star and the moon about the the barycenter
@@ -74,7 +74,7 @@ def t1(t0,p,ecosw,esinw):
 #t0: time of mid transit
 def t0(e,w,p,tp):
     '''Reparameterizes t0 in terms of e,w,p,tp'''
-    t0 = tp-p*np.sqrt(1-e**2)/(2*Pi)*(e*np.sin(1.5*Pi-w)/(1+e*np.cos(1.5*Pi-w)) - 2/np.sqrt(1-e**2)*atan2(np.sqrt(1-e**2)*np.tan(0.75*Pi-0.5*w), (1+e)))
+    t0 = tp-p*np.sqrt(1-e**2)/(2*Pi)*(-e*np.cos(w)/(1-e*np.sin(w)) - 2/np.sqrt(1-e**2)*atan2(np.sqrt(1-e**2)*np.tan(0.75*Pi-0.5*w), (1+e)))
     return t0
 
 def def_T(e,P,w,a,i):
